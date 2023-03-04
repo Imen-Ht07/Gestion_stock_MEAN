a46fab4adb090d7a70382a8f825f64c182d2ad17
Imen Hadj Taieb <115839915+Imen-Ht07@users.noreply.github.com>
2023-03-04 14:15:35 +0100

    Update database.js

--- Backend de GS/database.js
+++ Backend de GS/database.js
@@ -1,5 +1,5 @@
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://ImenHT:Bac.2020@cluster0.qjhwp.mongodb.net/gestionstock")
+mongoose.connect("mongodb+srv://user:psw@cluster0.qjhwp.mongodb.net/gestionstock")
 .then(bd => console.log("database connected")) 
 .catch(console.error());
